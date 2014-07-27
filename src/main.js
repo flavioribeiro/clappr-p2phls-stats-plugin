58847b197434fc21411f3e590720bf69fd94b5ac
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-07-26 21:25:48 -0300

    define a template for stats box (closes #1)

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ class P2PHLSStats extends UIPlugin {
   get name() { return 'p2phlsstats' }
   get tagName() { return 'div' }
   get type() { return "ui" }
+  get template() { return JST.p2phlsstats }
   get attributes() {
     return {'data-p2phlsstats': '',
             'class': 'p2phlsstats'}
@@ -21,17 +22,6 @@ class P2PHLSStats extends UIPlugin {
 
   addListeners() {
     Mousetrap.bind('ctrl+s', () => this.showOrHide());
-  }
-
-  content() {
-    return 'bemtv p2p/hls stats<br><br> \
-            chunks from p2p: 4<br> \
-            chunks from cdn: 7<br> \
-            chunks sent: 1<br> \
-            swarm size: 4<br> \
-            bitrate: 1264kb<br> \
-            state: playing_buffering<br> \
-            dropped frames: 100'
   }
 
   showOrHide() {
@@ -52,9 +42,20 @@ class P2PHLSStats extends UIPlugin {
     this.showing = false
   }
 
+  templateWithData() {
+    return this.template({
+      chunksFromCDN: 10,
+      chunksFromP2P: 2,
+      chunksSent: 3,
+      swarmSize: 2,
+      currentBitrate: 1264,
+      state: "PLAYING_BUFFERING",
+    })
+  }
+
   render() {
     var style = Styler.getStyleFor(this.name)
-    this.$el.html(this.content())
+    this.$el.html(this.templateWithData())
     this.container.$el.append(style)
     this.container.$el.append(this.$el)
     return this
