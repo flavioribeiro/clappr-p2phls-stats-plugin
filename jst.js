fa8c4b267f6ea825b45ef8c2f77d247a6b3ffc38
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-07-27 15:14:04 -0300

    css perfumery :lipstick:

--- jst.js
+++ jst.js
@@ -6,7 +6,7 @@ module.exports = {
 
   CSS: {
     
-      'p2phlsstats': '@font-face{font-family:StatsFont;src:url(assets/visitor.ttf) format("truetype")}.p2phlsstats[data-p2phlsstats]{font-family:StatsFont;position:absolute;text-align:left;z-index:3001;top:20px;left:20px;font-smooth:never;-webkit-font-smoothing:none;background-color:rgba(0,0,0,.7);color:#fff;border-radius:3px;width:155px;height:130px;letter-spacing:.01em;padding-top:11px;font-size:10px;padding-left:5px}.p2phlsstats[data-p2phlsstats] p{color:#fff;text-align:center;margin-bottom:10px}.p2phlsstats[data-p2phlsstats] span{color:#fff;display:block;text-align:left;margin-bottom:3px}.p2phlsstats[data-p2phlsstats] span.stats-status{display:inline}',
+      'p2phlsstats': '@font-face{font-family:StatsFont;src:url(assets/visitor.ttf) format("truetype")}.p2phlsstats[data-p2phlsstats]{font-family:StatsFont;position:absolute;text-align:left;z-index:3001;top:20px;left:20px;font-smooth:never;-webkit-font-smoothing:none;background-color:rgba(0,0,0,.7);color:#fff;border-radius:3px;width:155px;height:140px;padding-top:11px;font-size:10px;padding-left:5px}.p2phlsstats[data-p2phlsstats] p{color:#fff;text-align:center;margin-bottom:5px}.p2phlsstats[data-p2phlsstats] span{color:#fff;display:block;text-align:left;margin-bottom:3px;line-height:1em}.p2phlsstats[data-p2phlsstats] span.stats-status{display:inline}',
     
   }
 };
